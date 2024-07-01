fb5e07f9f2ca99476952e0f54cfe2a7375a12dab
CHANTILLY2023 <2553502012@qq.com>
2024-07-01 21:05:32 +0800

    Update MAMIFNet.py

--- MAMIFNet.py
+++ MAMIFNet.py
@@ -6,7 +6,7 @@ import math
 import sys
 from mmcv.cnn import build_norm_layer
 sys.path.insert(0, '../../')
-from lib.pvtv2 import pvt_v2_b0, pvt_v2_b1, pvt_v2_b2, pvt_v2_b2_li, pvt_v2_b3, pvt_v2_b4, pvt_v2_b5
+from pvtv2 import pvt_v2_b0, pvt_v2_b1, pvt_v2_b2, pvt_v2_b2_li, pvt_v2_b3, pvt_v2_b4, pvt_v2_b5
 
 import numpy as np
 import cv2
